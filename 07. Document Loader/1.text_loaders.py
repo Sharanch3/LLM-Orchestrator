6c25685a8c1c02b47a1dcf485e0ac220e0ba9726
Sharanch <saranchmukhia@gmail.com>
2026-06-19 21:50:00 +0530

    add txt_loader

---
+++ 07. Document Loader/1.text_loaders.py
@@ -0,0 +1,32 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V3.1",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+prompt = PromptTemplate(
+    template="Write a summary for the following poem- \n{poem}",
+    input_variables=['poem']
+)
+
+parser = StrOutputParser()
+
+
+loader = TextLoader('cricket.txt', encoding='utf-8')
+
+docs = loader.load()
+
+
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs[0].page_content}))
