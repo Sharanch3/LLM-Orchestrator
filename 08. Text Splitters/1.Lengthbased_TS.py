6391f2a818506439b5ed4e949c7e7f73c5f513c2
Sharanch <saranchmukhia@gmail.com>
2026-06-21 17:53:32 +0530

    add lengthbased_ts

---
+++ 08. Text Splitters/1.Lengthbased_TS.py
@@ -0,0 +1,18 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+
+
+loader = PyPDFLoader('dl-curriculum.pdf')
+
+docs =loader.load()
+
+
+splitter = CharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 0,
+    separator=''
+)
+
+result = splitter.split_documents(docs)
+
+print(result[0].page_content)
